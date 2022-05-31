6c79991b10cd87976a4cb25472231058363420d9
Azretshakirov <92852391+Azretshakirov@users.noreply.github.com>
2022-05-31 10:52:54 +0300

    Update register.php

--- register.php
+++ register.php
@@ -34,8 +34,8 @@
                         $mail->isSMTP();                                            //Send using SMTP
                         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
                         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                        $mail->Username   = 'YOUR_EMAIL_HERE';                     //SMTP username
-                        $mail->Password   = 'YOUR_PASSWORD_HERE';                               //SMTP password
+                        $mail->Username   = 'bekjanm166@gmail.com';                     //SMTP username
+                        $mail->Password   = 'bekjan123a)';                               //SMTP password
                         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
                         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
@@ -45,7 +45,7 @@
 
                         
                         $mail->isHTML(true);                                  //Set email format to HTML
-                        $mail->Subject = 'no reply';
+                        $mail->Subject = 'Link for verify';
                         $mail->Body    = 'Сылка для потверждения<b><a href="http://localhost/login/?verification='.$code.'">http://localhost/login/?verification='.$code.'</a></b>';
 
                         $mail->send();
